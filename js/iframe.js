fc3007e94087af73fbd5a2757d48b8c1e200c25f
Kali <aieternal@outlook.com>
2022-05-15 10:28:42 -0400

    iframe

---
+++ js/iframe.js
@@ -0,0 +1,11 @@
+function changeFrameHeight(){
+     var ifm= document.getElementById("iframepage"); 
+     ifm.height=document.documentElement.clientHeight;
+
+    }
+
+window.οnresize=function(){  
+     changeFrameHeight();  
+
+    } 
+
